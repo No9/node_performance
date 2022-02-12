5d1e3d5a72a8ba1248b1ad0db1c566539f761923
Anthony Whalley <anton@venshare.com>
2022-02-12 10:00:28 +0000

    refactor: move perf out of main container

--- cpu_bound/application.js
+++ cpu_bound/application.js
@@ -2,11 +2,10 @@
 
 const express = require('express')
 const http = require('http')
-const probe = require('probe-mon')
 
 const FIB_MAX = 40;
 
-let fibonacci = function(n) {
+let fibonacci = function (n) {
     return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
 }
 
@@ -14,10 +13,7 @@ let fibonacci = function(n) {
 let count = 0;
 let app = express();
 
-//probe
-//app.use(probe());
-
-let memoization = function(fn) {
+let memoization = function (fn) {
     let cache = {};
     return (num) => {
         if (typeof cache[num] !== 'number') {
@@ -30,18 +26,18 @@ let memoization = function(fn) {
 
 let memo = memoization(fibonacci);
 
-let warm_up = function() {
+let warm_up = function () {
     for (let i = 0; i < FIB_MAX; i++) {
         memo(i);
     }
 }();
 
-app.get('/fast', function(req, res, next) {
+app.get('/fast', function (req, res, next) {
     res.send('fast');
     count++;
 });
 
-app.get('/fib', function(req, res, next) {
+app.get('/fib', function (req, res, next) {
     let n = Math.floor(Math.random() * FIB_MAX) + 1;
 
     res.send({
@@ -58,14 +54,14 @@ app.get('/ugly', function ugly_func(req, res, next) {
     var values = [1, "a", 2, "b", 3, "c", 4, "d"];
     var s = "";
     for (var i in values) {
-      s += values[i].toString();
+        s += values[i].toString();
     }
 
-    res.send({'response': s});
+    res.send({ 'response': s });
     count++;
 });
 
-app.get('/sample', function(req, res, next) {
+app.get('/sample', function (req, res, next) {
     res.send({
         'number_request_attended': count
     });
@@ -73,6 +69,6 @@ app.get('/sample', function(req, res, next) {
 
 let httpServer = http.createServer(app);
 
-httpServer.listen(8080, function() {
+httpServer.listen(8080, function () {
     console.log('listening in ', 8080)
 });
